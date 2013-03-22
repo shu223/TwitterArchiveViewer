7d1f44b359b174aa212a82b7a8810fabb70f7f32
shu223 <shuichi0526@gmail.com>
2013-03-22 22:15:53 +0900

    Initial Commit

---
+++ TwitterArchiveViewer/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TwitterArchiveViewer
+//
+//  Created by shuichi on 13/03/22.
+//  Copyright (c) 2013年 Shuichi Tsutsumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TwitterArchiveViewer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TwitterArchiveViewer
+//
+//  Created by shuichi on 13/03/22.
+//  Copyright (c) 2013年 Shuichi Tsutsumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
